52c894b6457a135ff8638f3247ad8b51c74bea39
Aditya Raj <adityaraj5138@gmail.com>
2026-05-13 10:41:15 +0530

    Time: 106 ms (29.31%), Space: 77.7 MB (72.87%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,22 +1,47 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
         int n = heights.length;
-        Stack<Integer> ans = new Stack<>();
-        int maxArea =0;
+        Stack<Integer> st = new Stack<>();
+        int [] nse =new int[n];    // nse jo hai right to left (next smallest element ) 
+        int [] pse = new int [n];  // pse jo hai left to right(Privious smallest elem)
 
-        for(int i = 0; i< n; i++){
-           while (!ans.isEmpty() && heights[ans.peek()] >= heights[i]){
-            int height = heights[ans.pop()];
-            int width = ans.isEmpty() ? i : i- ans.peek() - 1;
-            maxArea = Math.max(maxArea , height * width);
+        st.push(n -1); // last index as it is push kar diya stack me
+
+        // Caluating nse[]
+        nse[ n-1] = n;   // n esliye ku ki ek jyada liye hai      
+
+        for(int i = n-2; i>=0; i--){ // jab first elemnt as it is push huwa esliye next elemnt se start huwa
+           while (st.size()>0 && heights[st.peek()] >= heights[i]){ //heights[ans.peek()] means st ke under index fill kar rhe hai 
+            st.pop();          
            } 
-           ans.push(i) ;
+           if(st.size() == 0) nse[i] = n;
+           else nse[i] = st.peek();
+           st.push(i);
         }
-        while(!ans.isEmpty()){
-            int height = heights[ans.pop()];
-            int width = ans.isEmpty() ? n : n-ans.peek()-1;
-            maxArea = Math.max(maxArea , height * width);
+        // empting stack kiya again;
+        while(st.size()>0) st.pop();
+
+        // Calculate pse[]
+        st.push(0);
+        pse[0] = -1;
+        for(int i = 1; i<=n-1; i++){
+            while(st.size()>0 && heights[st.peek()] >= heights[i]){
+                st.pop();
+            // iska mtlb hai kya stack me heights per jo value hai kya usse bara hai agar hai toh pop karo
+
+            }
+            if(st.size() == 0) pse[i] = -1;
+            else pse[i] = st.peek();
+            st.push(i);
         }
-        return maxArea;
+               
+    
+       // Maximum area of reactangle 
+        int max = -1;
+        for(int i= 0; i< n; i++){
+            int area = heights[i] * (nse[i] - pse[i] - 1);
+            max = Math.max(max, area);
+        }
+        return max;
     }
 }
